6555e1ddca98880f2e4bcbf2441a967e75d8a742
BenWhitehead <BenWhitehead@users.noreply.github.com>
2017-10-12 15:09:21 -0700

    Update launcher fixture to allow install url config from env.

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 import time
 
@@ -6,9 +7,32 @@ import pytest
 
 import dcos_launch.config
 import dcos_launch.util
+import yaml
+from dcos_launch import util
 from dcos_test_utils import logger
 
 logger.setup('DEBUG')
+
+log = logging.getLogger(__name__)
+
+
+def _write_yaml(y: dict, path: str) -> None:
+    try:
+        with open(path, "w") as f:
+            return yaml.safe_dump(y, f)
+    except yaml.YAMLError as ex:
+        raise util.LauncherError('InvalidYaml', None) from ex
+    except FileNotFoundError as ex:
+        raise util.LauncherError('MissingConfig', None) from ex
+
+
+def _merge_launch_config_with_env(launch_config_path: str) -> None:
+    installer_url = os.getenv("TEST_LAUNCH_CONFIG_INSTALLER_URL", None)
+    if installer_url is not None:
+        initial_config = dcos_launch.config.load_config(launch_config_path)
+        if 'installer_url' not in initial_config:
+            initial_config['installer_url'] = installer_url
+            _write_yaml(initial_config, installer_url)
 
 
 @pytest.fixture(scope='session')
@@ -38,23 +62,24 @@ def launcher(create_cluster, cluster_info_path):
         a launch info JSON for an existing cluster
     - TEST_CREATE_CLUSTER: can be `true` or `false`. If `true`, a new cluster
         will be created for this test
-    - TEST_CLUSTER_INFO_PATH: path where the cluster info will be info JSON
-        will be dumped for future manipulation
+    - TEST_CLUSTER_INFO_PATH: path where the cluster info JSON will be dumped
+        for future manipulation
     """
     # Use non-strict validation so that info JSONs with extra fields do not
     # raise errors on configuration validation
+    launch_config_path = os.environ['TEST_LAUNCH_CONFIG_PATH']
     if create_cluster:
-        launcher = dcos_launch.get_launcher(
-            dcos_launch.config.get_validated_config(os.environ['TEST_LAUNCH_CONFIG_PATH']))
+        _merge_launch_config_with_env(launch_config_path)
+        launcher = dcos_launch.get_launcher(dcos_launch.config.get_validated_config(launch_config_path))
         info = launcher.create()
         with open(cluster_info_path, 'w') as f:
             json.dump(info, f)
         # basic wait to account for initial provisioning delay
-        time.sleep(180)
+        time.sleep(int(os.getenv("INITIAL_SLEEP", "180")))
         launcher.wait()
     else:
         try:
-            launcher = dcos_launch.get_launcher(json.load(open(os.environ['TEST_LAUNCH_CONFIG_PATH'], 'r')))
+            launcher = dcos_launch.get_launcher(json.load(open(launch_config_path, 'r')))
             launcher.wait()
         except dcos_launch.util.LauncherError:
             raise AssertionError(

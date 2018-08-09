b5cf152dd8bbeed8257be992029c372fbfd83aa8
Kevin Miller-Conley <kmiller-conley@mesosphere.com>
2018-08-09 12:21:50 -0700

    Added a str conversion for json usage

--- test_upgrade.py
+++ test_upgrade.py
@@ -409,7 +409,7 @@ def setup_workload(dcos_api_session, dcoscli, viptalk_app, viplisten_app, health
     time.sleep(180)
 
     # Preserve the current quantity of words from the Kafka job so we can
-    kafka_job_words = json.loads(dcoscli.exec_command("dcos kafka topic offsets mytopicC".split()))[0]["0"]
+    kafka_job_words = json.loads(str(dcoscli.exec_command("dcos kafka topic offsets mytopicC".split())))[0]["0"]
 
     # TODO(branden): We ought to be able to deploy these apps concurrently. See
     # https://mesosphere.atlassian.net/browse/DCOS-13360.
@@ -576,6 +576,6 @@ class TestUpgrade:
             assert dcos_api_session.marathon.check_app_instances(framework_ids[package], 1, True, False) is True
 
         # Preserve the current quantity of words from the Kafka job so we can
-        kafka_job_words_post_upgrade = json.loads(dcoscli.exec_command("dcos kafka topic offsets mytopicC".split()))[0]["0"]
+        kafka_job_words_post_upgrade = json.loads(str(dcoscli.exec_command("dcos kafka topic offsets mytopicC".split())))[0]["0"]
 
         assert len(kafka_job_words_post_upgrade) > len(kafka_job_words)
